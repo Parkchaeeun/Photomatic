c42f0d69a836ff0485fba0374e44d9c02bf75b96
Parkchaeeeun <wlsflwns12@naver.com>
2018-10-18 01:53:13 +0900

    UI수정

--- app/src/main/java/com/google/sample/cloudvision/MainActivity.java
+++ app/src/main/java/com/google/sample/cloudvision/MainActivity.java
@@ -59,7 +59,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity{
-    private static final String CLOUD_VISION_API_KEY = "AIzaSyAEdUH91wgoQem2sKyh2NDD15Pag650810";
+    private static final String CLOUD_VISION_API_KEY = "";
     public static final String FILE_NAME = "temp.jpg";
     private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
     private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
